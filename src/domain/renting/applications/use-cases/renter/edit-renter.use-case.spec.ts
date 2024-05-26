1ba1240ddc59469edf7d3ad80f5a45be4de4deba
Luiz Verissimo <luizverissimosouza@gmail.com>
2024-05-26 14:08:34 -0300

    fix: renter's test

--- src/domain/renting/applications/use-cases/renter/edit-renter.use-case.spec.ts
+++ src/domain/renting/applications/use-cases/renter/edit-renter.use-case.spec.ts
@@ -1,7 +1,7 @@
 import { UniqueEntityID } from '@/shared/interfaces/entities/unique-entity-id'
 import { EditRenterUseCase } from './edit-renter.use-case'
 import { InMemoryRentersRepository } from 'test/repositories/in-memory-renters-repository'
-import { Renter } from '@/domain/renting/models/entities/renter'
+import { makeRenter } from 'test/factories/make-renter'
 
 let inMemoryRentersRepository: InMemoryRentersRepository
 let sut: EditRenterUseCase
@@ -12,17 +12,9 @@ describe('Edit a renter', () => {
     sut = new EditRenterUseCase(inMemoryRentersRepository)
   })
 
-  it('should edit a renter', async () => {
+  it("should edit a renter's name", async () => {
     inMemoryRentersRepository.items.push(
-      Renter.create(
-        {
-          name: 'luiz',
-          email: 'luiz@gmail.com',
-          phone: '+5548997121754',
-          photo: 'user01',
-        },
-        new UniqueEntityID('primeiroid')
-      )
+      makeRenter({}, new UniqueEntityID('primeiroid'))
     )
     const result = await sut.execute({
       id: 'primeiroid',
@@ -33,27 +25,17 @@ describe('Edit a renter', () => {
     expect(inMemoryRentersRepository.items[0].name).toEqual('luiz verissimo')
   })
 
-  it('should not create a renter without name', async () => {
+  it("should edit a renter's email", async () => {
+    inMemoryRentersRepository.items.push(
+      makeRenter({}, new UniqueEntityID('primeiroid'))
+    )
+
     const result = await sut.execute({
-      name: '',
+      id: 'primeiroid',
       email: 'luiz@gmail.com',
-      phone: '+5548997121754',
-      photo: 'user01',
     })
 
     expect(result.isLeft).toBeTruthy
-    expect(inMemoryRentersRepository.items.length).toBe(0)
-  })
-
-  it('should not create a renter without email', async () => {
-    const result = await sut.execute({
-      name: 'luiz',
-      email: '',
-      phone: '+5548997121754',
-      photo: 'user01',
-    })
-
-    expect(result.isLeft).toBeTruthy
-    expect(inMemoryRentersRepository.items.length).toBe(0)
+    expect(inMemoryRentersRepository.items[0].email).toEqual('luiz@gmail.com')
   })
 })
